e1fc681aa56e3a6f862e5729ec5b7b4988d48db4
kms617 <krysia.codes@gmail.com>
2014-08-22 11:20:12 -0400

    using Gemfile

--- server.rb
+++ server.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
-require 'pry'
+require 'redis'
+require 'json'
 require 'csv'
 enable :sessions
 
@@ -26,6 +27,32 @@ end
   checked_urls
 end
 
+def get_connection
+  if ENV.has_key?("REDISCLOUD_URL")
+    Redis.new(url: ENV["REDISCLOUD_URL"])
+  else
+    Redis.new
+  end
+end
+
+def find_articles
+    redis = get_connection
+    serialized_articles = redis.lrange("slacker:articles", 0, -1)
+
+    articles = []
+
+    serialized_articles.each do |article|
+      articles << JSON.parse(article, symbolize_names: true)
+    end
+
+    articles
+end
+
+def save_article(url, title, description)
+  article = {url: url, title: title, description: description}
+
+  redis = get_connection
+  redis.rpush("slacker:articles", article.to_json)
 ####################################################
 #                   ROUTES
 ####################################################
